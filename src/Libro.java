91d8bf65c1ee05afb668c71a7b6625617a67e7b2
Guillermina Gatti <yimina777@gmail.com>
2026-04-17 03:48:14 -0300

    Agregando ejercicios de clase 11-12

--- src/Libro.java
+++ src/Libro.java
@@ -1,5 +1,6 @@
 public class Libro {
-    private final int CODIGO; //CONSTANTES
+    private final int CODIGO; //CONSTANTES (puede ser double, int, long)
+    private final int LIMITE_STOCK_PROMO=30;
     private String titulo;
     private String autor;
     private int stock;
@@ -8,14 +9,15 @@ public class Libro {
     private char sector;
 
     //constructor
-    public Libro (int CODIGO, String titulo, String autor, int stock, double costo, char sector) {
-        this.CODIGO=CODIGO; //El codigo es una constate ahora
-        this.titulo=titulo;
-        this.autor=autor;
-        this.stock=stock;
-        this.costo=costo;
-        this.precioVenta=costo*1.25;
-        this.sector=sector;
+    public Libro(int CODIGO, String titulo, String autor, int stock, double costo, char sector) {
+        this.CODIGO = CODIGO; //El codigo es una constate ahora
+        this.titulo = titulo;
+        this.autor = autor;
+        this.stock = stock;
+        this.costo = costo;
+        this.precioVenta = costo * 1.25;
+        this.sector = sector;
+        aplicanPromosAlPrecioVenta();
     }
 
     //consultar
@@ -29,46 +31,109 @@ public class Libro {
 //    }
 
     //ver solo los ultimos 3 digitos del codigo
-    public String getCodigoBreve(){
-        String codigoTexto= String.valueOf(this.CODIGO) ;
-        return codigoTexto.substring(codigoTexto.length()-3);
+    public String getCodigoBreve() {
+        String codigoTexto = String.valueOf(this.CODIGO);
+        return codigoTexto.substring(codigoTexto.length() - 3);
+    }
+
+    public void setSector(char sector) {
+        this.sector = sector;
+        this.aplicanPromosAlPrecioVenta();
+    }
+
+
+    //CODIGO DE AGUS
+//    public void aplicanPromosAlPrecioVenta(){
+//
+//        this.precioVenta = costo*1.25; // 1250 el costo era 1000
+//
+//        String categoriaPromo="bazar";
+//
+//        //son acumulables las promos pero que sea >= al costo tiene prioridad
+//        if(this.stock > 15 && categoriaPromo.equals(this.categoria)){
+//            this.setPrecioVenta(this.precioVenta*=0.85); //1000 //1065
+//        }else if(this.stock > 15 || categoriaPromo.equals(this.categoria)){
+//            this.setPrecioVenta(this.precioVenta*=0.9);
+//        }
+//
+//    }
+    public void aplicanPromosAlPrecioVenta() {
+        //Cambié el programa en el proceso porque tenia un problema porque el precio de venta era menor al costo.
+        //Precio base desde el costo original
+        double nuevoPrecio = this.costo * 1.25;
+        //Aplicamos el primer descuento (5%)
+        if (this.stock > this.LIMITE_STOCK_PROMO) {
+            nuevoPrecio = nuevoPrecio * 0.95;
+        }
+        //Aplicamos el segundo descuento (2%)
+        if (this.sector == 'c') {
+            nuevoPrecio = nuevoPrecio * 0.98;
+        }
+        //Actualizamos usando el metodo
+        this.setPrecioVenta(nuevoPrecio);
+    }
+
+    public double getDescuento(){
+        double precioSinDescuento = this.costo * 1.25;
+        double descuento = precioSinDescuento - this.precioVenta;
+        return descuento;
     }
 
     //Get=mostrar y set=actualizar
 
     //Calcular el stock
-    public void setStock(int stock){
-        if(stock>=0) this.stock=stock;
+    public void setStock(int stock) {
+        if (stock >= 0) this.stock = stock;
         else System.out.println("no se modifico el stock, el mismo debe ser mayor a 0");
     }
 
     //Mostrar el stock
-    public int getStock(){
+    public int getStock() {
         return this.stock;
     }
 
     //hay stock disponible?
-    public boolean hayStock(){
+    public boolean hayStock() {
         return this.stock > 0;
     }
 
     //Ganancia obtenida
-    public double gananciaObtenida(){
+    public double gananciaObtenida() {
         return this.precioVenta - this.costo;
     }
 
-    public double getCosto(){
+    public double getCosto() {
         return this.costo;
     }
 
-    public void setPrecioVenta(double precioVenta, char sector) {
-        if (this.costo< precioVenta) this.precioVenta= precioVenta;
+    public void setPrecioVenta(double precioVenta) {
+        if (this.costo < precioVenta) this.precioVenta = precioVenta;
         else System.out.println("No puede ingresar un precio de venta menor al costo.");
-        if (this.sector=='c') this.precioVenta = precioVenta*0.9;
     }
 
-    public String getAutor(){
-        return this.autor=autor;
+    public double getPrecioVenta() {
+        return precioVenta;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "CODIGO=" + CODIGO +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor +
+                ", costo=" + costo +
+                ", precioVenta=" + precioVenta +
+                ", stock=" + stock +
+                ", sector='" + sector + '\'' +
+                '}';
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,84 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        Libro libro1= new Libro(123, "Orgullo y prejuicio", "Jane Austen", 3, 120, 'c');
 
-        System.out.println(libro1.getAutor()+" ");
+        //Inicializar objetos
+        Libro libro1= new Libro(12371241, "Orgullo y prejuicio", "Jane Austen", 100, 10000, 'c');
+        Libro libro2= new Libro(12411237, "Ciudades de papel", "John Green", 100, 8000, 'a');
 
-        libro1.setStock(-100);
-        libro1.setStock(100);
+//        System.out.println("El autor de "+libro1.getTitulo()+" es "+libro1.getAutor()+" ");
+//        System.out.println("El autor de "+libro2.getTitulo()+" es "+libro2.getAutor()+" ");
+//        System.out.println("El costo del libro: "+libro1.getTitulo()+" es de $"+libro1.getCosto());
 
-        System.out.println("El stock es: "+libro1.getStock());
+        //Consultar su disponibilidad
+//        System.out.println("El stock es del libro:"+libro2.getTitulo()+" es de: "+libro2.getStock());
 
+        //Actualizar stock
+//        libro1.setStock(-100);//Lanza el mensaje de que el stock debe ser mayor a cero
+//        libro1.setStock(100);
+//
+//        System.out.println("El stock es: "+libro1.getStock()); //Muestra el stock actual
 
+        //Calcular la ganancia obtenida por unidad
+//        System.out.println("Las ganancias obtenida son de: "+libro1.gananciaObtenida()+" por unidad");
+
+        //NO PUEDE MODIFICARSE EL COSTO
+        //Por eso no esta el metodo setCosto
+//        System.out.println("El precio de venta del libro "+libro1.getTitulo()+" es de "+libro1.getPrecioVenta()+" y el costo es de "+libro1.getCosto());
+
+        //Ver el precio de venta
+//        System.out.println("El precio de venta del libro "+libro2.getTitulo()+" es de "+libro2.getPrecioVenta());
+//        System.out.println("El precio de venta del libro "+libro1.getTitulo()+" es de "+libro1.getPrecioVenta());
+
+        //Descuento sobre el precio de venta
+        //Agregué este metodo porque queria saber el descuento
+//        System.out.println("El descuento del libro: "+libro1.getTitulo()+" es de "+ libro1.getDescuento());
+
+        //CODIGO INMUTABLE
+//        System.out.println("El codigo del libro "+libro1.getTitulo()+" es: "+libro1.getCODIGO());
+//        System.out.println("El codigo breve del libro "+libro1.getTitulo()+" es: "+libro1.getCodigoBreve());
+
+        //<Se requiere una función en la clase principal que reciba los datos separados por coma para registrar
+        // un nuevo libro e incluirlo en un arreglo de Libros. Visualizar la información del Array luego.>
+
+        //Definimos un arreglo para almacenar LIBROS
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de libros a almacenar: ");
+        int n = sc.nextInt();
+
+        Libro[] biblioteca = new Libro[n];
+        registrarLibrosEnLista(biblioteca);
+    }
+    public static void registrarLibrosEnLista(Libro[] lista) {
+        Scanner scanner = new Scanner(System.in);
+        int contador = 0;
+
+        while (contador < lista.length) {
+            System.out.println("No escribas comillas dobles ni simples al ingresar los datos");
+            System.out.println("Ingresa los datos separados por coma: " + "codigo,titulo,autor,stock,costo,sector");
+
+            String entrada = scanner.nextLine();
+            String[] datos = entrada.split(",");
+
+            // Conversion de tipos
+            int codigo = Integer.parseInt(datos[0].trim());
+            String titulo = datos[1].trim();
+            String autor = datos[2].trim();
+            int stock = Integer.parseInt(datos[3].trim());
+            double costo = Double.parseDouble(datos[4].trim());
+            char sector = datos[5].trim().charAt(0);
+
+            // Crear y guardar objeto
+            lista[contador] = new Libro(codigo, titulo, autor, stock, costo, sector);
+            contador++;
+//            scanner.nextLine(); // Limpiar buffer
+        }
+
+        //ARRAY FINAL
+        System.out.println("Libros registrados:");
+        System.out.println(Arrays.toString(lista));
     }
 }
